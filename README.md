b376d69e376dc78a4c07e8a1f6232d533f9a9510
Robin Quintero <rohaquinlop301@gmail.com>
2024-02-10 19:01:08 -0500

    feat(back): add output to XML file option
    - Add the option to output the results to a XML file.
    - Update the README.md to include the new option.
    - Update the README.md to include the -m 0 option to
    disable the exit with error.

--- README.md
+++ README.md
@@ -23,7 +23,12 @@ To run **complexipy** you can use the following command:
 <b>complexipy</b> path/to/directory         # Use complexipy to analyze a specific directory and any subdirectories
 <b>complexipy</b> path/to/file.py           # Use complexipy to analyze a specific file
 <b>complexipy</b> path/to/file.py -m 20     # Use the -m option to set the maximum congnitive complexity, default is 15
+<b>complexipy</b> path/to/directory -m 0    # Set the maximum cognitive complexity to 0 to disable the exit with error
+<b>complexipy</b> path/to/directory -o      # Use the -o option to output the results to a XML file, default is False
 </pre>
+
+If the cognitive complexity of a file is greater than the maximum cognitive, then
+the return code will be 1 and exit with error, otherwise it will be 0.
 
 For example, given the following file:
 
--- complexipy/main.py
+++ complexipy/main.py
@@ -1,10 +1,13 @@
 from pathlib import Path
 from complexipy import rust
+import os
 from rich.console import Console
 from rich.table import Table
 import time
 import toml
 import typer
+import xml.etree.ElementTree as ET
+import xml.dom.minidom
 
 root_dir = Path(__file__).resolve().parent.parent
 app = typer.Typer(name="complexipy")
@@ -16,6 +19,7 @@ version = toml_file["package"]["version"]
 def main(
     path: str,
     max_complexity: int = typer.Option(15, "--max-complexity", "-c", help="The maximum complexity allowed, set this value as 0 to set it as unlimited."),
+    output: bool = typer.Option(False, "--output", "-o", help="Output the results to a XML file."),
 ):
     has_success = True
     is_dir = Path(path).is_dir()
@@ -26,16 +30,37 @@ def main(
         files = rust.main(path, is_dir, max_complexity)
     execution_time = time.time() - start_time
     console.print("Analysis completed! :tada:")
+
+    # Output to XML
+    if output:
+        invocation_path = os.getcwd()
+        xml_file = ET.Element("complexity")
+        for file in files:
+            file_xml = ET.SubElement(xml_file, "file")
+            name = ET.SubElement(file_xml, "name")
+            name.text = file.file_name
+            file_path = ET.SubElement(file_xml, "path")
+            file_path.text = file.path
+            file_complexity = ET.SubElement(file_xml, "complexity")
+            file_complexity.text = str(file.complexity)
+        xml_str = ET.tostring(xml_file, encoding="utf-8")
+        dom = xml.dom.minidom.parseString(xml_str)
+        pretty_xml = dom.toprettyxml(indent="  ")
+        with open(f"{invocation_path}/complexipy.xml", "w") as file:
+            file.write(pretty_xml)
+        console.print(f"Results saved to {invocation_path}/complexipy.xml")
+
     # Summary
     table = Table(title="Summary", show_header=True, header_style="bold magenta", show_lines=True)
+    table.add_column("Path")
     table.add_column("File")
-    table.add_column("Cognitive Complexity")
+    table.add_column("Complexity")
     for file in files:
         if file.complexity > max_complexity and max_complexity != 0:
-            table.add_row(f"[green]{file.file_name}[/green]", f"[red]{file.complexity}[/red]")
+            table.add_row(f"{file.path}", f"[green]{file.file_name}[/green]", f"[red]{file.complexity}[/red]")
             has_success = False
         else:
-            table.add_row(f"[green]{file.file_name}[/green]", f"[blue]{file.complexity}[/blue]")
+            table.add_row(f"{file.path}", f"[green]{file.file_name}[/green]", f"[blue]{file.complexity}[/blue]")
     console.print(table)
     console.print(f"{len(files)} files analyzed in {execution_time:.4f} seconds")
 
